53f16055bf4dc428781a6dbd659b1e34e70cb1ce
xgreyskiesx <greysdawn@gmail.com>
2020-07-29 18:37:38 -0400

    Fix args order in messageDelete

--- bot/events/messageDelete.js
+++ bot/events/messageDelete.js
@@ -1,4 +1,4 @@
-module.exports = async (bot, message) => {
+module.exports = async (message, bot) => {
 	await bot.stores.responsePosts.delete(message.channel.guild.id, message.channel.id, message.id);
 	await bot.stores.formPosts.delete(message.channel.guild.id, message.channel.id, message.id);
 }
