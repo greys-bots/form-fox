1f0698ca8ce2cc7c9777ecb49868b49628e5100f
greysdawn <greysdawn@gmail.com>
2022-07-27 15:26:50 -0400

    Autodm fixes

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -1,14 +1,14 @@
 module.exports = async (member, bot) => {
 	if(member.user.dmChannel) {
 		var existing = await bot.stores.openResponses.get(member.user.dmChannel.id);
-		if(existing) return;
+		if(existing?.id) return;
 	}
 
 	var cfg = await bot.stores.configs.get(member.guild.id);
 	if(!cfg?.autodm) return;
 
 	var form = await bot.stores.forms.get(member.guild.id, cfg.autodm);
-	if(!form) return;
+	if(!form?.id) return;
 
 	try {
 		await bot.handlers.response.startResponse({
